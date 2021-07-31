8095c6b0276325b2e24fb255d5ff5c3d98c97722
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 16:59:44 -0700

    new

---
+++ project-apex/Assets/Scripts/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Controls the happenings for a particular level.
+/// </summary>
+public class LevelController : ApexMonoBehaviour
+{
+    [Header("---Settings---")]
+    [SerializeField]
+    private int nextLevelIndex = 1;
+
+    [Header("---Resources---")]
+    [SerializeField]
+    private IntVariable levelTracker;
+
+    private void Start()
+    {
+        levelTracker.Value = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevelIndex);
+    }
+}
