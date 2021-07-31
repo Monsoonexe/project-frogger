fe983a602dfe9eb7ae3c1248031c1347c3ad3bb3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 07:42:39 -0700

    input controller

--- project-apex/Assets/Scripts/PlayerInputController.cs
+++ project-apex/Assets/Scripts/PlayerInputController.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerInputController : ApexMonoBehaviour
+[SelectionBase]
+public class PlayerInputController : ApexMobile
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("---Settings---")]
+    public Vector3 gravityVector = new Vector3(0, -9.81f, 0);
+
+    public float groundCheckRadius = 1.0f;
+    public LayerMask groundLayerMask;
+    public float groundCheckRayLength = 0.15f;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    private FroggerMobile characterController;
+
+    [SerializeField]
+    private Transform groundCheckPoint;
+
+    [Header("---Inputs---")]
+    [SerializeField]
+    private StringVariable horizontalAxisName;
+
+    [SerializeField]
+    private StringVariable vertivalAxisName;
+
+    /// <summary>
+    /// Is the mobile contacting the ground within a threshold?
+    /// </summary>
+    public bool IsGrounded { get; private set; }
+
+    private void Update()
     {
-        
+        //get inputs
+        var playerInput = new Vector2
+        (
+            Input.GetAxis(horizontalAxisName),
+            Input.GetAxis(vertivalAxisName)
+        );
+
+        characterController.Move(playerInput);
     }
 
-    // Update is called once per frame
-    void Update()
+    //private Vector2 GetInputs()
+    //{
+    //    var playerInput = new Vector2
+    //    (
+    //        Input.GetAxis(horizontalAxisName),
+    //        Input.GetAxis(vertivalAxisName)
+    //    );
+
+    //    return playerInput;
+    //}
+
+    private bool CheckIfGrounded()
     {
-        
+        var rayOrigin = myTransform.TransformPoint(
+            groundCheckPoint.position);
+
+        IsGrounded = Physics.SphereCast(rayOrigin,
+            groundCheckRadius, gravityVector,
+            out RaycastHit hitInfo, groundCheckRayLength,
+            groundLayerMask);
+
+        return IsGrounded;
     }
 }
