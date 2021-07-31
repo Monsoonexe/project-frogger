9e25a3ac6eb3f3ddedab39d91102d9287da707e3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 09:11:24 -0700

    new trigger volume

---
+++ project-apex/Assets/Scripts/TriggerVolume.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerVolume : ApexMonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Empty means 'any'.")]
+    protected string reactToTag = "Player";
+
+    /// <summary>
+    /// Running total of times this has been triggered.
+    /// </summary>
+    [Tooltip("Running total of times this has been triggered.")]
+    public int triggerCount = 0;
+
+    [SerializeField]
+    protected UnityEvent enterEvent = new UnityEvent();
+
+    [SerializeField]
+    protected UnityEvent exitEvent = new UnityEvent();
+
+    [ContextMenu("ForceCollide()")]
+    public void ForceCollide()
+    {
+        ++triggerCount;
+        enterEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (string.IsNullOrEmpty(reactToTag) 
+            || col.gameObject.CompareTag(reactToTag))
+        {
+            ++triggerCount;
+            enterEvent.Invoke();
+        }
+    }
+
+    protected void OnTriggerExit(Collider col)
+    {
+        if (string.IsNullOrEmpty(reactToTag) 
+            || col.gameObject.CompareTag(reactToTag))
+        {
+            exitEvent.Invoke();
+        }
+    }
+}
