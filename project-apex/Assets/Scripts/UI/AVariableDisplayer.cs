661436cac1c7e3fd86b4b10a30a13eb74009e06e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 06:59:29 -0700

    new ui scripts

---
+++ project-apex/Assets/Scripts/UI/AVariableDisplayer.cs
@@ -0,0 +1,13 @@
+﻿
+public abstract class AVariableDisplayer<T> : VariableDisplayer
+    where T : AVariable
+{
+    protected T TypedVariable => (T)targetData;
+
+    protected override void Reset()
+    {
+        SetDevDescription("Observes a " + 
+            typeof(T).Name + " Variable and displays its value on events.");
+    }
+}
+
---
+++ project-apex/Assets/Scripts/UI/VariableDisplayer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// Observes a Variable and displays its value on events.
+/// </summary>
+public class VariableDisplayer : ApexMonoBehaviour
+{
+    [Header("---Resources---")]
+    [SerializeField]
+    protected AVariable targetData;
+
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    protected TextMeshProUGUI textElement;
+
+    protected virtual void Reset()
+    {
+        SetDevDescription("Observes a Variable and displays its value on events.");
+    }
+
+    protected virtual void OnEnable()
+    {
+        SubscribeToEvents();
+        UpdateUI();
+    }
+
+    protected virtual void OnDisable()
+    {
+        UnsubscribeFromEvents();
+    }
+
+    protected virtual void SubscribeToEvents()
+    {
+        targetData.onValueChangedEvent.AddListener(UpdateUI);
+    }
+
+    protected virtual void UnsubscribeFromEvents()
+    {
+        targetData.onValueChangedEvent.RemoveListener(UpdateUI);
+    }
+
+    public virtual void UpdateUI()
+    {
+        textElement.text = targetData.ToString();
+    }
+}
