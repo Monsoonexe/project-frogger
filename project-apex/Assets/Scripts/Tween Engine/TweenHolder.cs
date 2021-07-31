87f0723c1d0f8593f5bcf3c4e2bebaf40d999872
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 16:58:14 -0700

    new

---
+++ project-apex/Assets/Scripts/Tween Engine/TweenHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenHolder : ApexMonoBehaviour
+{
+    private void Reset()
+    {
+        SetDevDescription("I am a utility class to hold tweens" +
+            " so you don't have to!");
+    }
+}
