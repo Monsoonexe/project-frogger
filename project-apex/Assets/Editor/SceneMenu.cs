17c1cd9dd165ebf40dbb5a7c7827a19f0cc015ce
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 06:58:53 -0700

    scene menu

---
+++ project-apex/Assets/Editor/SceneMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace RichPackage.Editor
+{
+    public static class SceneMenu
+    {
+        #region Menu Items
+
+        [MenuItem("Scenes/Reload Current Scene")]
+        public static void ReloadCurrentScene()
+            => EditorSceneManager.OpenScene(
+                SceneManager.GetActiveScene().name); //don't prompt to save
+
+        #endregion
+
+        #region Internal Functions
+
+        /// <summary>
+        /// Load a scene and prompt User to save the scene.
+        /// </summary>
+        public static void LoadScene(string scenePath)
+        {
+            //prompt to save
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+            //actually change scenes
+            EditorSceneManager.OpenScene(scenePath);
+        }
+
+        /// <summary>
+        /// Load a scene and prompt User to save the scene.
+        /// </summary>
+        public static void LoadScene(Scene scene)
+            => LoadScene(scene.path);
+
+        #endregion
+
+        //----------EXAMPLE ENTRY-----------
+        //[MenuItem("Scenes/Start Menu Scene")]
+        //private static void LoadStartMenuScene()
+        //   => LoadScene("Assets/Scenes/StartMenu.unity");
+    }
+
+}
