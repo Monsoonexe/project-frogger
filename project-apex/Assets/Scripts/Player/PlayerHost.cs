38e2a54af17f25af190cc51a7db7894b6955fe85
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 16:59:31 -0700

    new player scripts

---
+++ project-apex/Assets/Scripts/Player/PlayerHost.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Helps manage the Player.
+/// </summary>
+public class PlayerHost : ApexMonoBehaviour
+{
+    public static PlayerHost Instance { get; private set; }
+
+    [Header("---Settings---")]
+    [SerializeField]
+    [Tooltip("Time after respawn that player should be inactive.")]
+    private float respawnResumeDelay = 1.5f;
+
+    [SerializeField]
+    private float deathInterval = 0.5f;
+
+    [Header("---Resources---")]
+    [SerializeField]
+    private IntVariable playerDeathCounter;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    private Transform playerMobileHandle;
+
+    [SerializeField]
+    private PlayerInputController playerInput;
+
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    private Transform respawnPoint;
+
+    private void Reset()
+    {
+        SetDevDescription("Helps manage the Player.");
+    }
+
+    private void Awake()
+    {
+        //easy singleton
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Singleton Error!", this);
+        }
+    }
+
+    public void KillPlayer()
+    {
+        playerInput.enabled = false;//cut player controls
+        //TODO immediately play SFX and screen shake
+        ++playerDeathCounter.Value;
+        ApexTweens.InvokeAfterDelay(RespawnPlayer, deathInterval);
+    }
+
+    public void RespawnPlayer()
+    {
+        playerMobileHandle.position = respawnPoint.position;
+
+        //re-enable input after some time.
+        ApexTweens.InvokeAfterDelay(
+            () => playerInput.enabled = true, respawnResumeDelay);
+    }
+}
---
+++ project-apex/Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+[SelectionBase]
+public class PlayerInputController : ApexMobile
+{
+    [Header("---Settings---")]
+    public Vector3 gravityVector = new Vector3(0, -9.81f, 0);
+
+    public float groundCheckRadius = 1.0f;
+    public LayerMask groundLayerMask;
+    public float groundCheckRayLength = 0.15f;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    private FroggerMobile characterController;
+
+    [SerializeField]
+    private Transform groundCheckPoint;
+
+    [Header("---Inputs---")]
+    [SerializeField]
+    private StringVariable horizontalAxisName;
+
+    [SerializeField]
+    private StringVariable vertivalAxisName;
+
+    /// <summary>
+    /// Is the mobile contacting the ground within a threshold?
+    /// </summary>
+    public bool IsGrounded { get; private set; }
+
+    private void Update()
+    {
+        //snap inputs instead of damping
+        var horInput = Input.GetAxis(horizontalAxisName);
+        if (horInput > 0) horInput = 1;
+        else if (horInput < 0) horInput = -1;
+
+        //snap inputs instead of damping
+        var forwardInput = Input.GetAxis(vertivalAxisName);
+        if (forwardInput > 0) forwardInput = 1;
+        else if (forwardInput < 0) forwardInput = -1;
+
+        var playerInput = new Vector3(
+            horInput, 0, forwardInput);
+        //Debug.Log(playerInput);
+
+        characterController.Move(playerInput);
+    }
+
+    //private Vector2 GetInputs()
+    //{
+    //    var playerInput = new Vector2
+    //    (
+    //        Input.GetAxis(horizontalAxisName),
+    //        Input.GetAxis(vertivalAxisName)
+    //    );
+
+    //    return playerInput;
+    //}
+
+    private bool CheckIfGrounded()
+    {
+        var rayOrigin = myTransform.TransformPoint(
+            groundCheckPoint.position);
+
+        IsGrounded = Physics.SphereCast(rayOrigin,
+            groundCheckRadius, gravityVector,
+            out RaycastHit hitInfo, groundCheckRayLength,
+            groundLayerMask);
+
+        return IsGrounded;
+    }
+}
