894b5566cb3b1a03eb94c424e14ef849bd66ef43
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 16:58:57 -0700

    new timer

---
+++ project-apex/Assets/Scripts/SpawnOnTimer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Every x seconds, spawn a new hazard.
+/// </summary>
+[RequireComponent(typeof(GameObjectPool))]
+public class SpawnOnTimer : ApexMonoBehaviour
+{
+    [Header("---Settings---")]
+    [SerializeField]
+    private Vector2 randomTimeIntervalBounds = new Vector2(1.5f, 4.0f);
+
+    [SerializeField]
+    private bool shouldOverrideDirection = true;
+
+    [SerializeField]
+    private Vector3 directionOverride = new Vector3(1, 0, 0);
+
+    [SerializeField]
+    [Tooltip("-1 means immortal.")]
+    [Min(-1)]
+    private float hazardLifetime = 10.0f;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    private Transform spawnPoint;
+
+    private GameObjectPool hazardPool;
+    private float nextSpawnTime;
+
+    private void Reset()
+    {
+        SetDevDescription("I spawn items from a pool at time intervals.");
+    }
+
+    private void Awake()
+    {
+        hazardPool = GetComponent<GameObjectPool>();
+        InitNextSpawn();
+    }
+
+    private void InitNextSpawn()
+    {
+        //randomly select the next time to spawn
+        nextSpawnTime = ApexGameController.UpTime
+            + randomTimeIntervalBounds.RandomRange();
+
+        //wait that much time, then call this method
+        ApexTweens.InvokeAfterDelay(SpawnNextHazard, nextSpawnTime);
+    }
+
+    private void SpawnNextHazard()
+    {
+        //(instantiate)
+        var newHazard = hazardPool.Depool(
+            spawnPoint.position, Quaternion.identity);
+
+        //pool is empty, consider raising limits
+        if (!newHazard) return;
+
+        if(shouldOverrideDirection)
+        {
+            var hazard = newHazard.GetComponent<ProjectileHazard>();
+            if (hazard != null)
+                hazard.moveVector = directionOverride;
+        }
+
+        //reclaim after some time (unless immortal)
+        if(hazardLifetime > 0)
+            ApexTweens.InvokeAfterDelay(
+                () => hazardPool.Enpool(newHazard), hazardLifetime);
+
+        InitNextSpawn();//chain forever
+    }
+
+}
