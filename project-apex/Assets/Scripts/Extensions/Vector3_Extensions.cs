35acdeccceb409bd8e2c2a9789f69bad003ab16a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 07:41:52 -0700

    extension helper

---
+++ project-apex/Assets/Scripts/Extensions/Vector3_Extensions.cs
@@ -0,0 +1,17 @@
+﻿//credit to Jason Weimann and Unity College
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public static class Vector3_Extensions
+{
+    public static Vector3 WithX(this Vector3 a, float x)
+        => new Vector3(x, a.y, a.z );
+
+    public static Vector3 WithY(this Vector3 a, float y)
+        => new Vector3(a.x, y, a.z);
+
+    public static Vector3 WithZ(this Vector3 a, float z)
+        => new Vector3(a.x, a.y, z);
+}
