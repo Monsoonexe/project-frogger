88d2900947392e71b95a2f303895efd5f3d74506
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 02:34:13 -0700

    added extensions

--- project-apex/Assets/Scripts/Static Classes/Extensions/Vector2_Extensions.cs
+++ project-apex/Assets/Scripts/Static Classes/Extensions/Vector2_Extensions.cs
@@ -4,4 +4,16 @@ public static class Vector2_Extensions
 {
     public static float RandomRange(this Vector2 range)
         => Random.Range(range.x, range.y);
+
+    /// <summary>
+    /// Returns true if x is in inverval [range.x, range.y].
+    /// </summary>
+    public static bool IsWithin(this Vector2 range, float x)
+        => (x >= range.x && x <= range.y);
+
+    /// <summary>
+    /// Returns true if x is in inverval [range.x, range.y].
+    /// </summary>
+    public static bool IsWithin(this Vector2Int range, int x)
+        => (x >= range.x && x <= range.y);
 }
