2f0e9ddb8505900712b99a21f1edb93f1765c897
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 00:55:05 -0700

    rigged evenets and triggers

--- project-apex/Assets/Scripts/UI/ScreenTransitionUI.cs
+++ project-apex/Assets/Scripts/UI/ScreenTransitionUI.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using UnityEngine;
 
+//clarifications
+using Random = UnityEngine.Random;
+
 /// <summary>
 /// Handles cool fade to/from black screen transitions.
 /// Uses static events instead of singleton pattern.
 /// </summary>
 public class ScreenTransitionUI : ApexMonoBehaviour
 {
-    public static Action<string> TriggerTransitionEvent;
+    public enum ETransitionType
+    {
+        /// <summary>
+        /// Black overtakes to block scene.
+        /// </summary>
+        IN,
+
+        /// <summary>
+        /// Black goes away to reveal scene.
+        /// </summary>
+        OUT
+    }
+    public static Action<ETransitionType> TriggerRandomTransitionEvent;
 
     [Header("---Scene Refs---")]
     [SerializeField]
     private Animator myAnimator;
+
+    [Header("---Animator Messages---")]
+    [SerializeField]
+    private string animateInMessage = "In";
+
+    [SerializeField]
+    private string animateOut_X_Message = "Out_X";
+    public string AnimateOut_X_Message { get => animateOut_X_Message; } //readonly
+
+    [SerializeField]
+    private string animateOut_Y_Message = "Out_Y";
+    public string AnimateOut_Y_Message { get => animateOut_Y_Message; } //readonly
 
     private void Reset()
     {
@@ -20,17 +47,35 @@ public class ScreenTransitionUI : ApexMonoBehaviour
 
     private void OnEnable()
     {
-        TriggerTransitionEvent += TriggerTransition;
+        TriggerRandomTransitionEvent += TriggerRandomTransitionHandler;
     }
 
     private void OnDisable()
     {
-        TriggerTransitionEvent -= TriggerTransition;
+        TriggerRandomTransitionEvent -= TriggerRandomTransitionHandler;
     }
 
-    private void TriggerTransition(string transitionName)
+    private void TriggerRandomTransitionHandler(ETransitionType transition)
     {
-        //TODO input validation and constants in custom editors
-        myAnimator.SetTrigger(transitionName);
+        string animatorMessage = null;
+
+        //determine desired
+        switch (transition)
+        {
+            case ETransitionType.IN:
+                animatorMessage = animateInMessage;
+                break;
+            case ETransitionType.OUT:
+                animatorMessage = (Random.Range(0.0f, 1.0f) <= 0.5f)//coin flip
+                    ? animateOut_X_Message : animateOut_Y_Message;
+                break;
+            default:
+                Debug.LogWarning("Case not accounted for: " + transform.ToString());
+                break;
+        }
+
+        //validate transition
+        if(!string.IsNullOrEmpty(animatorMessage))
+            myAnimator.SetTrigger(animatorMessage);
     }
 }
