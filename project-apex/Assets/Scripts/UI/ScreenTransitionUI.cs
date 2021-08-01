77c915dd49469434d861dd6abadd51b7815b7e15
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 00:13:48 -0700

    new

---
+++ project-apex/Assets/Scripts/UI/ScreenTransitionUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Handles cool fade to/from black screen transitions.
+/// Uses static events instead of singleton pattern.
+/// </summary>
+public class ScreenTransitionUI : ApexMonoBehaviour
+{
+    public static Action<string> TriggerTransitionEvent;
+
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    private Animator myAnimator;
+
+    private void Reset()
+    {
+        SetDevDescription("Handles cool fade to/from black screen transitions.");
+    }
+
+    private void OnEnable()
+    {
+        TriggerTransitionEvent += TriggerTransition;
+    }
+
+    private void OnDisable()
+    {
+        TriggerTransitionEvent -= TriggerTransition;
+    }
+
+    private void TriggerTransition(string transitionName)
+    {
+        //TODO input validation and constants in custom editors
+        myAnimator.SetTrigger(transitionName);
+    }
+}
