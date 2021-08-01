9f4233673c7fdca6f3ae7fba929b7a104b36a78d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 00:09:55 -0700

    new

---
+++ project-apex/Assets/Scripts/DestroyLifetime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Destroys parent GameObject after given time.
+/// </summary>
+public class DestroyLifetime : ApexMonoBehaviour
+{
+    [Header("---Settings---")]
+    [Min(0)]
+    public float lifeTime = 10;
+
+    private void Reset()
+    {
+        SetDevDescription("Destroys parent GameObject after given time.");
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
