69c1a986e84ae908b8991a83efca0b26dfb5c50d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 16:27:53 -0700

    new

---
+++ project-apex/Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// The level is won when the Player enters my volume.
+/// </summary>
+public class LevelGoal : ApexMonoBehaviour
+{
+    [Header("---Animation---")]
+    [SerializeField]
+    private float celebrationTimeInterval = 5.0f;
+
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    private LevelController levelController;
+
+    [Header("---Audio---")]
+    [SerializeField]
+    private AudioClip reachGoalClip;
+
+    //runtime values
+    private Coroutine winRoutine;
+
+    public bool IsAnimating { get => winRoutine != null; }
+
+    private void Reset()
+    {
+        SetDevDescription("The level is won when the Player enters my volume." +
+            "Anything else that should happen immediately upon victory can be " +
+            "rigged to TriggerVolume event.");
+    }
+
+    public void OnPlayerReachGoal()
+    {
+        //prevent spamming
+        if (IsAnimating) return;
+
+        winRoutine = StartCoroutine(WinRoutine());
+    }
+
+    private IEnumerator WinRoutine()
+    {
+        //TODO - celebrate!
+        //sound
+        reachGoalClip.Play();
+
+        //particle
+        //UI message
+
+        //wait a bit to soak it in
+        yield return new WaitForSeconds(celebrationTimeInterval);
+
+        //move to next scene
+        levelController.LoadNextLevel();
+
+        winRoutine = null;//clear flag
+    }
+}
