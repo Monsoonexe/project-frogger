ff889d63b76ecb475dfead7be29a9858375bf135
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-02 03:28:32 -0700

    helper mono

---
+++ project-apex/Assets/Scripts/UnityCallbackMono.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Can give Start() behaviour to non-Mono behaviour.
+/// </summary>
+public class UnityCallbackMono : ApexMonoBehaviour
+{
+    [Header("---Events---")]
+    public UnityEvent startEvent = new UnityEvent();
+
+    private void Reset()
+    {
+        SetDevDescription("Can give Start() behaviour to non-Mono behaviour.");
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+}
