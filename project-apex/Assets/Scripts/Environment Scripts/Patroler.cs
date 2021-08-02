4d05eb540e75dc187c3b83e556a349ae6ce06272
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 23:59:39 -0700

    new

---
+++ project-apex/Assets/Scripts/Environment Scripts/Patroler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Patroler : ApexMonoBehaviour
+{
+    [Header("---Settings---")]
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    [Tooltip("Handle on the thing that is to be moved.")]
+    private Transform mobileHandle;
+
+    [SerializeField]
+    private Transform[] points;
+
+    //runtime
+    private int waypointIndex;
+
+    private void Reset()
+    {
+        mobileHandle = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        var currentPosition = mobileHandle.position;
+        var targetPosition = points[waypointIndex].position;
+        var step = moveSpeed * ApexGameController.DeltaTime;
+
+        //combine all the above
+        var moveVector = Vector3.MoveTowards(
+            currentPosition, targetPosition, step);
+
+        mobileHandle.position = moveVector;
+
+        //determine if waypoint is complete
+        if(moveVector == currentPosition)
+        {   //loop waypoint index
+            waypointIndex = (++waypointIndex >= points.Length) 
+                ? 0 : waypointIndex;
+        }
+    }
+}
