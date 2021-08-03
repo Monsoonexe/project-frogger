8c35c2e139488a6c9f9afc32a3c7e09bd23988b5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-03 00:27:15 -0700

    new extension

---
+++ project-apex/Assets/Scripts/Static Classes/Extensions/GameObject_Extensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class GameObject_Extensions
+{
+    public static bool TryGetComponent<TComponent>(
+        this GameObject gameObject, out TComponent component)
+        where TComponent : Component
+    {
+        component = gameObject.GetComponent<TComponent>();
+        return component != null;
+    }
+}
