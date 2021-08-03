ebe60da5eb536e9aa61efc952bb6876a24b375ca
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-03 00:29:08 -0700

    new

---
+++ project-apex/Assets/Scripts/Player/TileDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TileDetector : ApexMonoBehaviour
+{
+    [SerializeField]
+    private RaycastDetector raycaster;
+
+    private void Reset()
+    {
+        raycaster = GetComponent<RaycastDetector>();
+    }
+
+    private void Awake()
+    {
+        raycaster.OnHitDetected += InterpretRaycastHit;
+    }
+
+    private void OnDestroy()
+    {
+        raycaster.OnHitDetected -= InterpretRaycastHit;
+    }
+
+    private void InterpretRaycastHit(in RaycastHit hitInfo)
+    {
+        if(hitInfo.collider.gameObject.TryGetComponent<ATile>(
+            out ATile theTile))
+        {
+            theTile.OnEnterTile();
+        }
+    }
+}
