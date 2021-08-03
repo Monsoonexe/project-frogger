bbcf93e0f9eeee153ed82c846d72f27ed827b728
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-03 03:02:29 -0700

    Update ApexGameController.cs

--- project-apex/Assets/Scripts/ApexGameController.cs
+++ project-apex/Assets/Scripts/ApexGameController.cs
@@ -43,7 +43,7 @@ public class ApexGameController : MonoBehaviour
 
     private void Start()
     {
-        Application.targetFrameRate = targetFrameRate;
+        Application.targetFrameRate = targetFrameRate.Value;
 
         //load data
         if (PlayerPrefs.HasKey(gameWinsCount.name))
