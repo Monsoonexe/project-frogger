6070379cadf3d6d9792000086f67139c53f59727
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-05 04:45:20 -0700

    Invert raises event. [Create]

--- project-apex/Assets/Scripts/ScriptableObjects/Scriptable Variables/BoolVariable.cs
+++ project-apex/Assets/Scripts/ScriptableObjects/Scriptable Variables/BoolVariable.cs
@@ -4,11 +4,13 @@
 /// 
 /// </summary>
 /// <seealso cref="IntVariable"/>
-[CreateAssetMenu()]
+[CreateAssetMenu(
+    fileName = "B_",
+    menuName = CREATE_ASSET_MENU_PATH + "bool")]
 public class BoolVariable : AVariable<bool>
 {
     public bool InvertedValue { get => !_value; }
-    public void Invert() => _value = !_value;
+    public void Invert() => Value = !_value;
 
     public override string ToString()
         => _value ? bool.TrueString : bool.FalseString;
