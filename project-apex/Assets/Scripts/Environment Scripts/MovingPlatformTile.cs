41749a018aaf1e9bb69e4d322b97db9b0dce72e3
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-05 14:58:32 -0700

    Update MovingPlatformTile.cs

--- project-apex/Assets/Scripts/Environment Scripts/MovingPlatformTile.cs
+++ project-apex/Assets/Scripts/Environment Scripts/MovingPlatformTile.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// More like "sticky" platform.
@@ -9,9 +8,6 @@ public class MovingPlatformTile : ATile
     [Header("---Settings---")]
     [SerializeField]
     private bool snapPlayerToCenter = false;
-
-    [SerializeField]
-    private float snapSpeed = 1.0f;
 
     [Header("---Prefab Refs---")]
     [SerializeField]
@@ -26,36 +22,6 @@ public class MovingPlatformTile : ATile
         platformTransform = GetComponent<Transform>();
     }
 
-    private void Update()
-    {
-        //TODO guard this better instead of every frame
-        //move player towards center point of object
-        if(CurrentTile == this && snapPlayerToCenter)
-        {
-        }
-    }
-
-    private IEnumerator MovePlayerToCenter()
-    {
-        const float duration = 1.0f; //only do this for 1 second
-        float runtime = 0.0f;
-        var playerHandle = PlayerHost.Instance.PlayerMobileHandle;
-        var currentPosition = playerHandle.localPosition;
-        var anchorPoint = Vector3.zero
-            .WithY(currentPosition.y);
-        do
-        {
-            var deltaTime = ApexGameController.DeltaTime;
-            playerHandle.localPosition = Vector3.MoveTowards(
-                currentPosition, anchorPoint,
-                 deltaTime * snapSpeed);
-
-            runtime += deltaTime;
-            yield return null;//next frame
-
-        } while (runtime < duration);
-    }
-
     public override void OnEnterTile()
     {
         base.OnEnterTile();
@@ -65,18 +31,17 @@ public class MovingPlatformTile : ATile
         playerHandle.SetParent(platformTransform);
 
         //handle snapping player
-        //if (snapPlayerToCenter)
-        //{
-        //    var anchorPoint = Vector3.zero
-        //        .WithY(playerHandle.localPosition.y);
-        //    playerHandle.localPosition = anchorPoint;
-        //}
+        if (snapPlayerToCenter)
+        {
+            var anchorPoint = Vector3.zero
+                .WithY(playerHandle.localPosition.y);
+            playerHandle.localPosition = anchorPoint;
+        }
     }
 
     public override void OnExitTile()
     {
         base.OnExitTile();
-
         //restore parent
         PlayerHost.Instance.PlayerMobileHandle.SetParent(cachedParent);
         cachedParent = null;
