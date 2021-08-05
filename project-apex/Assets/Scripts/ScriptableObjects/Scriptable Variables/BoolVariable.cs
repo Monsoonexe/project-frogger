fbeef0a94854095294bc82edc9ce226021c1dbff
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-05 04:04:46 -0700

    new

---
+++ project-apex/Assets/Scripts/ScriptableObjects/Scriptable Variables/BoolVariable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+/// <seealso cref="IntVariable"/>
+[CreateAssetMenu()]
+public class BoolVariable : AVariable<bool>
+{
+    public bool InvertedValue { get => !_value; }
+    public void Invert() => _value = !_value;
+
+    public override string ToString()
+        => _value ? bool.TrueString : bool.FalseString;
+}
