cdc537c0d5a6714acf05efcbd37072855d32aae7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-08 21:48:18 -0700

    new

---
+++ project-apex/Assets/Scripts/Tween Engine/ApexTween.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Nah, got wayy too complicated for such a small project.
+/// Don't re-create DOTween.
+/// </summary>
+public class ApexTween
+{
+    private Coroutine myRoutine;
+    private Transform myTransform;
+    public Action onComplete;
+    public Action onStart;
+}
